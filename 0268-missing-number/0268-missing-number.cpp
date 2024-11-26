17a5eedeeb02c1801ed6d6f3b98fb0a9e6cca416
Yukti Dave <yuktidave1212@gmail.com>
2024-11-26 18:58:33 +0530

    Time: 0 ms (100.00%), Space: 21.8 MB (5.44%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int xor1 = 0;
+        int xor2 = 0;
+        for(int i = 0; i < n; i++){
+            xor2 = xor2 ^ nums[i];
+            xor1 = xor1 ^ (i+1);
+        }
+        return xor1 ^ xor2;
+    }
+};
