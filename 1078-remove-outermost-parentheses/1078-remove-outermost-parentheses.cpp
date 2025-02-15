0ad6e719ed24a4673b0da06ff0f5c37a11dcc330
Yukti Dave <yuktidave1212@gmail.com>
2025-02-15 22:13:20 +0530

    Time: 0 ms (100%), Space: 9 MB (54.91%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int count = 0;
+        string ans = "";
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == ')') count--;
+            if(count != 0) ans.push_back(s[i]);
+            if(s[i] == '(') count++;
+        }
+        return ans;
+    }
+};
